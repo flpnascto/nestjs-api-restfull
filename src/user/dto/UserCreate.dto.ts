640be4c4f1fd502167d08b5efba6f5104580a485
Felipe Nascimento <flpnascto@gmail.com>
2023-07-05 11:29:18 -0300

    feat: add create user validation

---
+++ src/user/dto/UserCreate.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, Matches, MinLength } from 'class-validator';
+
+export class UserCreateDTO {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @MinLength(3, { message: 'O nome precisa ter pelo menos 3 caracteres' })
+  name: string;
+
+  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
+  email: string;
+
+  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#\$%\^&\*]).{6,}$/,
+    {
+      message:
+        'A senha precisa ter pelo menos 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial',
+    },
+  )
+  password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import UserRepository from './user.repository';
+import { UserCreateDTO } from './dto/UserCreate.dto';
 
 @Controller('/users')
 export default class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async create(@Body() user: any) {
+  async create(@Body() user: UserCreateDTO) {
     this.userRepository.save(user);
     return user;
   }
